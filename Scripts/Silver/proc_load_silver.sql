53b6f831dd55e59668ffc18effa351eaf018450f
Dedaldino Papelo <kelsonpapelo18@gmail.com>
2026-06-30 21:51:56 +0100

    Update proc_load_silver.sql

--- Scripts/Silver/proc_load_silver.sql
+++ Scripts/Silver/proc_load_silver.sql
@@ -1,1 +1,16 @@
+SELECT *
+FROM bronze.crm_sales_details
 
+SELECT cst_id, COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+
+SELECT prd_id, COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+
+SELECT *
+FROM silver.crm_cust_info
+
+SELECT *
+FROM silver.crm_prd_info
